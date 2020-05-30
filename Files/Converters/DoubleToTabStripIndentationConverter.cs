31b1c2c60125ba1ee83b09fa9bd1dc9c81fc4399
Luke Blevins <lukeblevins15@gmail.com>
2020-05-30 18:17:34 -0400

    Enable Dynamic Changes to TabStrip Margin

---
+++ Files/Converters/DoubleToTabStripIndentationConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Files.Converters
+{
+    class DoubleToTabStripIndentationConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return new Thickness((double)value, 0, 0, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return ((Thickness)value).Left;
+        }
+    }
+}
